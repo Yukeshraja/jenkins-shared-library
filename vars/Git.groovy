678cfaed7db1f201f234ce1d045060841a751899
Bibin Wilson <bibin@K9-MAC-008.local>
2018-12-18 21:22:01 +0530

    first shared library commit

--- vars/Git.groovy
+++ vars/Git.groovy
@@ -1,3 +1,4 @@
 def call(scmUrl, branch) {
+  echo "${scmUrl}"
   checkout([$class: 'GitSCM', branches: [[name: '*/${branch}']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: '${scmUrl}']]])
 }
