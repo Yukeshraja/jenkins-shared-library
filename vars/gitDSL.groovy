61c3025a46260e3c1dcca00b1313b1fa318fc28b
DevopsCube <admin@comtechies.com>
2018-12-20 21:54:22 +0530

    Update gitDSL.groovy

--- vars/gitDSL.groovy
+++ vars/gitDSL.groovy
@@ -1,6 +1,7 @@
 
 def call(config) {
     println config.scmUrl
+    println config.branch
     //checkout([$class: 'GitSCM', branches: [[name: "*/${config.branch}"]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: "${config.scmUrl}"]]])
 
 
