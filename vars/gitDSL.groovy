e18f23fa26d177c4a735b087d39361f316f54d2c
DevopsCube <admin@comtechies.com>
2018-12-20 21:51:39 +0530

    Update gitDSL.groovy

--- vars/gitDSL.groovy
+++ vars/gitDSL.groovy
@@ -1,9 +1,5 @@
 
-def call(body) {
-    def config= [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
+def call(config) {
     println config
     //checkout([$class: 'GitSCM', branches: [[name: "*/${config.branch}"]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: "${config.scmUrl}"]]])
 
